bdd3d17000cd8ce2c7929001c89b1ae75a1fcc6d
LAPTOP-OAPN857L\SUN <siripatsaya.d@mail.kmutt.ac.th>
2021-04-24 15:51:25 +0700

    ข้อที่6

---
+++ ข้อ6.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ConsoleApp10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Char Letters = Char.Parse(Console.ReadLine());
+            int Width = int.Parse(Console.ReadLine());
+            int Hight = int.Parse(Console.ReadLine());
+            int Number = int.Parse(Console.ReadLine());
+            int round = 1;
+
+            while (round <= Number)
+            {
+                for (int j = 1; j <= Hight; j++)
+                {
+                    for (int i = 1; i <= round * Width; i++)
+                    {
+                        Console.Write(Letters);
+                    }
+                    Console.WriteLine("");
+                }
+                round++;
+            }
+            Console.ReadLine();
+        }
+    }
+}
